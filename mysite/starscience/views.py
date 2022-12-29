92ddf99ad30ebdbddf23351a12c146231a8d7700
davi-canuto <davicanutogregorio@gmail.com>
2022-12-29 17:54:27 -0300

    improvement: improves article view

--- mysite/starscience/views.py
+++ mysite/starscience/views.py
@@ -1,14 +1,42 @@
-# from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import *
 
-# # Create your views here.
-# from django.http import HttpResponseRedirect
-# from django.urls import reverse
-# from django.shortcuts import render, get_object_or_404
-# from django.views import View
-# from .models import Tema, Pergunta, Resposta
+def index(request):
+    return render(request, 'index.html')
 
-# class HomeView(View):
-#     def get(self, request, *args, **kwargs):
-#         articles = Article.objects.all()
-#         context = {'articles': articles}
-#         return render(request, 'starscience/home.html', context)
+class ArticleViews:
+     def listArticle(request):
+        article_lists = Article.objects.all()
+        context = {'article_lists': article_lists}
+        return render(request, 'article/listArticle.html', context)
+
+    def formArticle(request):
+        return render(request, 'article/formArticle.html')
+
+    def saveArticle(request):
+        c = Article(title=request.POST['title'],
+                   description=request.POST['description'],
+                   url=request.POST['url'],
+                   likes=request.POST['likes'])
+        c.save()
+
+        return redirect('/listArticle')
+
+    def deleteArticle(request, id):
+        c = Article.objects.get(pk=id)
+        c.delete()
+        return redirect('/listArticle')
+
+    def detailArticle(request, id):
+        article = Article.objects.get(pk=id)
+        return render(request, 'article/formEditArticle.html', {'article': article} )
+
+    def updateArticle(request, id):
+        c = Article.objects.get(pk=id)
+        c.title = request.POST['title']
+        c.description = request.POST['description']
+        c.url = request.POST['url']
+        c.likes = request.POST['likes']
+        c.save()
+
+        return redirect('/listArticle')
