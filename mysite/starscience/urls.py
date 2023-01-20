2453d097fb331100305d16ad5f565ba3186274f5
davi-canuto <davicanutogregorio@gmail.com>
2023-01-20 07:31:28 -0300

    refact: encapsulated the view methods inside a class and organized the urls for the same

--- mysite/starscience/urls.py
+++ mysite/starscience/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import article_index, article_create, article_show, article_update, article_delete
+from .views import ArticleView
 
 urlpatterns = [
     # AS ARTICLE
-    path('articles/', article_index, name='article_index'),
-    path('articles/create/', article_create, name='article_create'),
-    path('articles/<int:id>/', article_show, name='article_show'),
-    path('articles/<int:id>/update/', article_update, name='article_update'),
-    path('articles/<int:id>/delete/', article_delete, name='article_delete'),
 
-    # # AS CATEGORY
+    path('articles/', ArticleView.article_index, name='article_index'),
+    path('articles/create/', ArticleView.article_create, name='article_create'),
+    path('articles/<int:id>/', ArticleView.article_show, name='article_show'),
+    path('articles/<int:id>/update/', ArticleView.article_update, name='article_update'),
+    path('articles/<int:id>/delete/', ArticleView.article_delete, name='article_delete'),
 ]
--- mysite/starscience/views.py
+++ mysite/starscience/views.py
@@ -1,46 +1,43 @@
 from django.shortcuts import render, redirect
 from .models import Article
 
-# REFERRING TO ARTICLE
+class ArticleView:
+    def article_index(request):
+        objects = Article.objects.all()
+        return render(request, 'articles/index.html', {'objects': objects})
 
-def article_index(request):
-    objects = Article.objects.all()
-    return render(request, 'articles/index.html', {'objects': objects})
+    def article_create(request):
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            description = request.POST.get('description')
+            url = request.POST.get('url')
+            likes = request.POST.get('likes')
 
-def article_create(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        url = request.POST.get('url')
-        likes = request.POST.get('likes')
+            Article.objects.create(title=title, description=description, url=url, likes=likes)
+            return redirect('article_index')
+        else:
+            return render(request, 'articles/create.html')
 
-        Article.objects.create(title=title, description=description, url=url, likes=likes)
-        return redirect('article_index')
-    else:
-        return render(request, 'articles/create.html')
+    def article_show(request, id):
+        obj = Article.objects.get(id=id)
+        return render(request, 'articles/show.html', {'object': obj})
 
-def article_show(request, id):
-    obj = Article.objects.get(id=id)
-    return render(request, 'articles/show.html', {'object': obj})
+    def article_update(request, id):
+        obj = Article.objects.get(id=id)
+        if request.method == 'POST':
+            obj.title = request.POST.get('title')
+            obj.description = request.POST.get('description')
+            obj.url = request.POST.get('url')
+            obj.likes = request.POST.get('likes')
+            obj.save()
+            return redirect('article_index')
+        else:
+            return render(request, 'articles/update.html', {'object': obj})
 
-def article_update(request, id):
-    obj = Article.objects.get(id=id)
-    if request.method == 'POST':
-        obj.title = request.POST.get('title')
-        obj.description = request.POST.get('description')
-        obj.url = request.POST.get('url')
-        obj.likes = request.POST.get('likes')
-        obj.save()
-        return redirect('article_index')
-    else:
-        return render(request, 'articles/update.html', {'object': obj})
-
-def article_delete(request, id):
-    obj = Article.objects.get(id=id)
-    if request.method == 'POST':
-        obj.delete()
-        return redirect('article_index')
-    else:
-        return render(request, 'articles/delete.html', {'object': obj})
-
-#  REFERRING TO CATEGORY
+    def article_delete(request, id):
+        obj = Article.objects.get(id=id)
+        if request.method == 'POST':
+            obj.delete()
+            return redirect('article_index')
+        else:
+            return render(request, 'articles/delete.html', {'object': obj})
