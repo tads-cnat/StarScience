8eb048d28fcc9c446f162cfc21f3093bc295bde5
Paulo Henrique Soares Lima e Silva <59665236+HeroDestiny@users.noreply.github.com>
2023-07-11 11:26:59 -0300

    Delete FrontEnd directory

--- FrontEnd/src/routes/cadastrar_categorias/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<h1>Cadastrar Categorias</h1>
-
-<ul>
-    <li><a href="/">StarScience</a></li>
-    <li><a href="/cadastrar_artigos">Cadastrar Artigos</a></li>
-    <li><a href="/listar_artigos">Listar Artigos</a></li>
-  </ul>
-
-<slot />
